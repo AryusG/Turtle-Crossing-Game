a7d2f25030cbc2d215847a440d1be484e569c266
AryusG <verdyg24@gmail.com>
2021-05-20 00:28:09 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkeypress(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.car_move()
+
+    # TODO detect when turtle collides with car, stop game if happens
+
+    # TODO detect when turtle reaches top, then return to starting position, increase car speed
+
+    # TODO Create scoreboard to track level
