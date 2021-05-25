31ec8507f1a2da4c019eafb7c8ace8f091c549e8
AryusG <verdyg24@gmail.com>
2021-05-25 20:47:44 +1000

    #3 Added a save file
    New data file to save highscores

--- scoreboard.py
+++ scoreboard.py
@@ -1,26 +1,30 @@
 from turtle import Turtle
-# FONT = ("Courier", 24, "normal")
+
+
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.level = 1
-        self.high_score = 1
+        with open("data.txt") as high_score:
+            self.high_score = int(high_score.read())
         self.hideturtle()
         self.penup()
         self.goto(-160, 260)
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        if self.level >= self.high_score:
-            self.high_score = self.level
         self.clear()
         self.write(f"Level: {self.level} Highscore: {self.high_score}", align='center', font=("Courier", 14, "bold"))
 
     def reset_scoreboard(self):
+        if self.level >= self.high_score:
+            with open("data.txt", "w") as data:
+                data.write(f"{self.level}")
         self.level = 1
         self.update_scoreboard()
+
 
     def increase_level(self):
         self.level += 1
